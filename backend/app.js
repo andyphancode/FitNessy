c3d1c35fe005c6216c5ac843da33ce00d444a262
andyphancode <andyphanpxh@gmail.com>
2024-04-04 20:14:52 -0700

    added some imports and middleware

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,10 @@
 
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const { authenticateJWT } = require("./middleware/auth");
 const jsonschema = require("jsonschema");
+
 
 const { NotFoundError } = require("./expressError");
 
@@ -11,7 +14,18 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.use(morgan("tiny"));
+app.use(authenticateJWT);
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to FitNessy backend." });
+});
+
 
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
