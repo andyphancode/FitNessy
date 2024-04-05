e7d4990c13abe27211e21aaba82f4868592be740
andyphancode <andyphanpxh@gmail.com>
2024-04-05 11:03:08 -0700

    homepage setup

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from '../assets/Logo.png'; 
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Welcome to FitNessy!</h1>
+      <h2>The easiest way to track your workouts.</h2>
+    </div>
+  );
+}
+
+export default Home;
