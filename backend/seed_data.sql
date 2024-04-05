0473b8767c7dbede13d3dfd3fde3395fd9ea932d
andyphancode <andyphanpxh@gmail.com>
2024-04-05 13:25:49 -0700

    added extra entry to create array

--- backend/seed_data.sql
+++ backend/seed_data.sql
@@ -2758,3 +2758,5 @@ INSERT INTO exercises (exercise_link, instructions, exercise_name, equipment, bo
 
 INSERT INTO user_exercises (username, exercise_id, exercise_date, rep1, rep2, rep3, rir1, rir2, rir3)
 VALUES ('testuser', '1', '2000-01-01', '5', '5', '5', '1', '1', '1');
+INSERT INTO user_exercises (username, exercise_id, exercise_date, rep1, rep2, rep3, rir1, rir2, rir3)
+VALUES ('testuser', '2', '2000-01-01', '2', '2', '2', '2', '2', '2');
