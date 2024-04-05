be78635697b719801881e3a69dd7a380b2cd5c76
andyphancode <andyphanpxh@gmail.com>
2024-04-05 03:11:11 -0700

    created authentication helper and middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -9,7 +9,7 @@ const { UnauthorizedError } = require("../expressError");
 /** Middleware: Authenticate user.
  *
  * If a token was provided, verify it, and, if valid, store the token payload
- * on res.locals (this will include the username and isAdmin field.)
+ * on res.locals (this will include the username field.)
  *
  * It's not an error if no token was provided or if the token is not valid.
  */
@@ -19,7 +19,7 @@ function authenticateJWT(req, res, next) {
     const authHeader = req.headers && req.headers.authorization;
     if (authHeader) {
       const token = authHeader.replace(/^[Bb]earer /, "").trim();
-      res.locals.user = jwt.verify(token, SECRET_KEY);
+      res.locals.user = jwt.verify(token,SECRET_KEY);
     }
     return next();
   } catch (err) {
