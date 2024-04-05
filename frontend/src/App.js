2bfd9fae668564dd1e5972432a6d119afc68a726
andyphancode <andyphanpxh@gmail.com>
2024-04-05 11:01:20 -0700

    set up app.js, stores exercises in localstorage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Routing from './routing/Routing';
+import FitNessyApi from './api/api';
+import {jwtDecode} from "jwt-decode";
+import useLocalStorage from "./useLocalStorage";
+import 'bootstrap/dist/css/bootstrap.css';
+import { useAuth } from './context/AuthContext';
 
 function App() {
+
+
+  const [currentUser, setCurrentUser] = useState(null);
+
+
+  useEffect(() => {
+    async function fetchAndStoreExercises() {
+      try {
+        const exercises = await FitNessyApi.getExercises();
+        localStorage.setItem('exercises', JSON.stringify(exercises));
+      } catch (error) {
+        console.error("Failed to fetch exercises:", error);
+        // handle the error in UI
+      }
+    };
+    fetchAndStoreExercises();    
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div style={{ display: 'flex' }}>
+          <Sidebar />
+          <main style={{ flexGrow: 1, padding: '20px' }}>
+            <Routing />
+          </main>
+      </div>
+    </Router>
   );
 }
 
