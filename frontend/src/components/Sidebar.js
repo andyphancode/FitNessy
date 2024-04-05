1c9c1648bbd0804711068fcf4c85b93ba681c18a
andyphancode <andyphanpxh@gmail.com>
2024-04-05 13:56:34 -0700

    Update Sidebar.js

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,7 +1,4 @@
-// src/components/Sidebar.js
-
-import { useContext } from "react";
-import { Drawer, List, ListItem, ListItemIcon, ListItemText, Box, Typography } from '@mui/material';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, Box } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import LoginIcon from '@mui/icons-material/Login';
 import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
@@ -21,7 +18,21 @@ function Sidebar() {
   function loggedOut() {
     return (
       <List>
-        {['Home', 'Login', 'Register'].map((text, index) => (
+        <ListItem
+          key="Home"
+          component={Link}
+          to={`/`}
+          sx={{
+            textDecoration: 'none', 
+            color: 'inherit',
+          }}
+        >
+          <ListItemIcon>
+            <HomeIcon/>
+          </ListItemIcon>
+          <ListItemText primary={"Home"}/>
+        </ListItem>
+        {['Login', 'Register'].map((text, index) => (
           <ListItem key={text} component={Link} to={`/${text.toLowerCase()}`}
           sx={{
             textDecoration: 'none', 
@@ -30,9 +41,8 @@ function Sidebar() {
           }}
           >
             <ListItemIcon>
-              {index === 0 && <HomeIcon />}
-              {index === 1 && <LoginIcon />}
-              {index === 2 && <AppRegistrationIcon />}
+              {index === 0 && <LoginIcon />}
+              {index === 1 && <AppRegistrationIcon />}
             </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
@@ -44,34 +54,48 @@ function Sidebar() {
   function loggedIn() {
     return (
       <List>
-        {['Home', 'Workouts'].map((text, index) => (
-          <ListItem key={text} component={Link} to={`/${text.toLowerCase()}`}
+        <ListItem
+          key="Home"
+          component={Link}
+          to={`/`}
           sx={{
             textDecoration: 'none', 
             color: 'inherit',
           }}
-          >
-            <ListItemIcon>
-              {index === 0 && <HomeIcon />}
-              {index === 1 &&<FitnessCenterIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      <ListItem
-        key="Logout"
-        sx={{
-          textDecoration: 'none',
-          color: 'inherit',
-          cursor: 'pointer' // Makes it clear this is clickable
-        }}
-        onClick={logout} // Calls the logout function on click
-      >
-        <ListItemIcon>
-          <LogoutIcon />
-        </ListItemIcon>
-        <ListItemText primary="Logout" />
-      </ListItem>
+        >
+          <ListItemIcon>
+            <HomeIcon/>
+          </ListItemIcon>
+          <ListItemText primary={"Home"}/>
+        </ListItem>
+        <ListItem
+          key="Workouts"
+          component={Link}
+          to={`/workouts`}
+          sx={{
+            textDecoration: 'none', 
+            color: 'inherit',
+          }}
+        >
+          <ListItemIcon>
+            <FitnessCenterIcon/>
+          </ListItemIcon>
+          <ListItemText primary={"Workouts"}/>
+        </ListItem>    
+        <ListItem
+          key="Logout"
+          sx={{
+            textDecoration: 'none',
+            color: 'inherit',
+            cursor: 'pointer' // Makes it clear this is clickable
+          }}
+          onClick={logout} // Calls the logout function on click
+        >
+          <ListItemIcon>
+            <LogoutIcon />
+          </ListItemIcon>
+          <ListItemText primary="Logout" />
+        </ListItem>
       </List>
     );
   };
