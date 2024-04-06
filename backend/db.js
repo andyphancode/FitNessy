bf403121d6f73a65e157b62db8873b158adb0c0d
andyphancode <andyphanpxh@gmail.com>
2024-04-05 18:02:30 -0700

    switched db.js set up for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,55 +1,4 @@
-// for development on personal PC
-"use strict";
-/** Database setup for FitNessy. */
-const { Client } = require("pg");
-const { getDatabaseUri } = require("./config");
-
-let db;
-
-
-
-
-try // windows route
-{
-  if (process.env.NODE_ENV === "production") {
-    db = new Client({
-      host: "/var/run/postgresql",
-      database: getDatabaseUri(),
-      ssl: {
-        rejectUnauthorized: false
-      }
-    });
-  } else {
-    db = new Client({
-      host: "/var/run/postgresql",
-      database: getDatabaseUri()
-    });
-  }
-  db.connect();
-
-  module.exports = db;
-} 
-catch // mac route
-{
-  if (process.env.NODE_ENV === "production") {
-    db = new Client({
-      // connectionString: `postgresql:///${getDatabaseUri()}`,
-      connectionString: getDatabaseUri(),
-      ssl: {
-        rejectUnauthorized: false
-      }
-    });
-  } else {
-    db = new Client({
-      // connectionString: `postgresql:///${getDatabaseUri()}`
-      connectionString: getDatabaseUri()
-    });
-  }
-  db.connect();
-
-  module.exports = db;
-}
-
+// // for development on personal PC
 // "use strict";
 // /** Database setup for FitNessy. */
 // const { Client } = require("pg");
@@ -57,22 +6,73 @@ catch // mac route
 
 // let db;
 
-// try {
+
+
+
+// try // windows route
+// {
 //   if (process.env.NODE_ENV === "production") {
 //     db = new Client({
-//       connectionString: process.env.DATABASE_URL,
+//       host: "/var/run/postgresql",
+//       database: getDatabaseUri(),
 //       ssl: {
 //         rejectUnauthorized: false
 //       }
 //     });
 //   } else {
 //     db = new Client({
+//       host: "/var/run/postgresql",
+//       database: getDatabaseUri()
+//     });
+//   }
+//   db.connect();
+
+//   module.exports = db;
+// } 
+// catch // mac route
+// {
+//   if (process.env.NODE_ENV === "production") {
+//     db = new Client({
+//       // connectionString: `postgresql:///${getDatabaseUri()}`,
+//       connectionString: getDatabaseUri(),
+//       ssl: {
+//         rejectUnauthorized: false
+//       }
+//     });
+//   } else {
+//     db = new Client({
+//       // connectionString: `postgresql:///${getDatabaseUri()}`
 //       connectionString: getDatabaseUri()
 //     });
 //   }
 //   db.connect();
-// } catch (error) {
-//   console.error("Error connecting to the database:", error);
+
+//   module.exports = db;
 // }
 
-// module.exports = db;
+"use strict";
+/** Database setup for FitNessy. */
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+
+let db;
+
+try {
+  if (process.env.NODE_ENV === "production") {
+    db = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    });
+  } else {
+    db = new Client({
+      connectionString: getDatabaseUri()
+    });
+  }
+  db.connect();
+} catch (error) {
+  console.error("Error connecting to the database:", error);
+}
+
+module.exports = db;
